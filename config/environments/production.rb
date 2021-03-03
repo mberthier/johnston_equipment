a1dcf72ec9b5aeebe59917851a4e232a2ad8f8eb
Maxime Berthier <berthiermaxime@orange.fr>
2021-03-02 18:51:34 -0800

    remove form for logic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'johnstonequipment.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-
-  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'johnstonequipment.herokuapp.com',
-    :enable_strattls_auto => true
-  }
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
