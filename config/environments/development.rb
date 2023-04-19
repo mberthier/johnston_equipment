d68be9253494ad63adde1fac3c581050c2347928
Benjamin Bouchet <contact@benjamin-bouchet.com>
2023-04-19 15:13:45 +0200

    serve_static_files in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Serve static files in development environment
+  config.serve_static_files = true
 end
