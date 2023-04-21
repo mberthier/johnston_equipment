6518c85c51c17bc3e08a34d38c68e32b0d2ebb77
Maxime Berthier <41864957+mberthier@users.noreply.github.com>
2023-04-21 11:27:22 -0700

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   has_many_attached :photos
   belongs_to :category
   belongs_to :sub_category, optional: true
-  validates :title, :description, :year, :price, :brand, :category, :photos, presence: true
+  validates :title, :description, :price, :brand, :category, :photos, presence: true
   # validates :sub_category, inclusion: { in: ["Forestry", "Flatbeds", "Vans", "Vocational","Highway tractors", "Cab and Chassis", "Construction", "Agriculture", "Engine parts", "Fuel and Hydraulic", "Tanks and Accessories", "Gauges", "Truck", "Equipment"] }
   # validates :category, inclusion: { in: ["Trucks", "Trailers", "Equipment", "Attachments", "Parts", "Misc"] }
 end
